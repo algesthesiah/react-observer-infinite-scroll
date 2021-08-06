132ee1f11d2c944f7d824e05bbd8d374d977bb60
Joey Leo <541877028@qq.com>
2021-08-06 15:40:31 +0800

    feat: 优化滚动加载 (#5)

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,16 @@ import React, {
   ForwardRefExoticComponent,
   memo,
 } from 'react';
-import { useIntersection, useUpdateEffect } from 'react-use'
-import { useDebounceFn, usePersistFn, usePrevious } from 'ahooks'
-import './index.css'
-const defaultIntersectionOption = { root: null, rootMargin: '0px', threshold: 1 }
+import { useIntersection, useUpdateEffect } from 'react-use';
+import { usePersistFn, usePrevious } from 'ahooks';
+import './index.css';
+
+const getDefaultIntersectionOption = (isInverse: boolean | undefined) => {
+  if (isInverse) {
+    return { root: null, rootMargin: '20px 0px 0px 0px', threshold: 1 };
+  }
+  return { root: null, rootMargin: '-20px 0px 0px 0px', threshold: 1 };
+};
 
 export interface InfiniteScrollOutRef {
   scrollToBottom?: null | (() => void);
@@ -28,33 +34,32 @@ export interface InfiniteScrollProps {
   loader: ReactNode;
   inverse?: boolean;
   dataLength: number;
-  immediate?: boolean;
   intersectionOption?: IntersectionObserverInit;
 }
 const InfiniteScroll: ForwardRefExoticComponent<InfiniteScrollProps> = memo(
-  forwardRef(({ children, next, style, hasMore, loader, dataLength, immediate, inverse, intersectionOption }, ref) => {
+  forwardRef(({ children, next, style, hasMore, loader, dataLength, inverse, intersectionOption }, ref) => {
     const _intersectionOption = {
-      ...defaultIntersectionOption,
+      ...getDefaultIntersectionOption(inverse),
       ...intersectionOption,
-    }
-    const [loadingShow, setLoadingShow] = useState(true)
-    const { run } = useDebounceFn(
-      () => {
-        next && next()
-      },
-      {
-        wait: 300,
-      }
-    )
-
+    };
     const stateRef = useRef({
-      isInit: false,
-    })
-    const intersectionRef = useRef<HTMLDivElement>(null)
-    const inverseViewRef = useRef<HTMLDivElement>(null)
+      // 可能一开始就进入了视口，需要过滤这种情况
+      initTime: 0,
+    });
+    const [loadingShow, setLoadingShow] = useState(true);
 
-    const intersection = useIntersection(intersectionRef, _intersectionOption)
-    const isIntoVIew = useMemo(() => intersection && intersection.intersectionRatio === 1, [intersection])
+    const intersectionRef = useRef<HTMLDivElement>(null);
+    const inverseViewRef = useRef<HTMLDivElement>(null);
+
+    const intersection = useIntersection(intersectionRef, _intersectionOption);
+    const isIntoVIew = useMemo(
+      () =>
+        intersection &&
+        intersection.intersectionRatio === 1 &&
+        stateRef.current.initTime !== 0 &&
+        intersection.time - stateRef.current.initTime > 300,
+      [intersection]
+    );
 
     const scrollToBottom = usePersistFn(() => {
       if (inverse) {
@@ -62,51 +67,50 @@ const InfiniteScroll: ForwardRefExoticComponent<InfiniteScrollProps> = memo(
           behavior: 'auto',
           block: 'end',
           inline: 'nearest',
-        })
+        });
       }
       intersectionRef?.current?.scrollIntoView({
         behavior: 'auto',
         block: 'end',
         inline: 'nearest',
-      })
-    })
+      });
+    });
     useImperativeHandle(ref, () => ({
       scrollToBottom,
-    }))
+    }));
 
     useUpdateEffect(() => {
-      if (!stateRef.current.isInit) {
-        // 初始化场景
-        stateRef.current.isInit = true
-        if (immediate) {
-          setLoadingShow(true)
-          run()
-          return
-        }
-        return
-      }
-      // 进入视图场景
       if (isIntoVIew && hasMore) {
-        setLoadingShow(true)
-        run()
+        setLoadingShow(true);
+        next();
       }
-    }, [isIntoVIew, hasMore])
+    }, [isIntoVIew, hasMore]);
 
-    const oldDataLength = usePrevious(dataLength)
+    const oldDataLength = usePrevious(dataLength);
+
     useUpdateEffect(() => {
-      if (oldDataLength === dataLength) {
-        setLoadingShow(false)
+      // 结束场景、过度场景
+      if ((oldDataLength === dataLength && !hasMore) || (hasMore && dataLength != oldDataLength)) {
+        setLoadingShow(false);
       }
-    }, [oldDataLength, dataLength])
+    }, [oldDataLength, dataLength, hasMore]);
+
+    useUpdateEffect(() => {
+      if (intersection?.time) {
+        if (stateRef.current.initTime === 0) {
+          stateRef.current.initTime = intersection?.time;
+        }
+      }
+    }, [stateRef, intersection]);
+
     return (
       <div className={inverse ? 'react-infinite-scroll-wrap inverse' : 'react-infinite-scroll-wrap'} style={style}>
         {inverse && <div ref={inverseViewRef} className="inverse-view"></div>}
         {children}
         <div ref={intersectionRef}></div>
-        <div className="inverse-fake"></div>
         {loadingShow && hasMore && (loader || 'loading...')}
       </div>
-    )
+    );
   })
-)
-export default InfiniteScroll
+);
+export default InfiniteScroll;
